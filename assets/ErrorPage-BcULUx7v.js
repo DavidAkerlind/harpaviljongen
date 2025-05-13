4e7bffb57c44f6414986b58b113fc50e38f053a2
DavidAkerlind <david.akerlind@icloud.com>
2025-05-13 17:06:36 +0200

    Updates

--- assets/ErrorPage-BcULUx7v.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CyMnDFZt.js";function a(){return r.jsx("section",{className:"page page-error"})}export{a as default};
---
+++ assets/NewsPage-DG5KzmHK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DZAZzQZP.js";function a(){return e.jsx("section",{className:"page page-news"})}export{a as default};
