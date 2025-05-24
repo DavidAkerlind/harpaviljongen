14372c656de54085f768ebd375f28f1c84a0eef8
DavidAkerlind <david.akerlind@icloud.com>
2025-05-24 21:58:22 +0200

    fix: Correct button text from "CHAMBRÈ" to "CHAMBRE" in HomePage; adjust NavBar and PageHeader positioning for improved layout

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -35,7 +35,7 @@ function HomePage() {
 			<section className="page__top-section">
 				<Button text="MENY" link="/menu" />
 				<Button text="ÖPPETTIDER" link="#openingHours" />
-				<Button text="CHAMBRÈ" link="/chambre" />
+				<Button text="CHAMBRE" link="/chambre" />
 			</section>
 			<Menu menuId="menu-lunch" img={hareLogo} />
 
